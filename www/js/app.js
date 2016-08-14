5b61101ffd730d5ca4f0361d80ce5e8662e6a804
Naguchi <lucrerakuri@gmail.com>
2016-08-15 00:12:02 +0900

    タイトルをセンタリング

--- www/js/app.js
+++ www/js/app.js
@@ -22,7 +22,7 @@ angular.module('starter', ['ionic', 'starter.controllers'])
   });
 })
 
-.config(function($stateProvider, $urlRouterProvider) {
+.config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
   $stateProvider
 
     .state('app', {
@@ -70,6 +70,8 @@ angular.module('starter', ['ionic', 'starter.controllers'])
   });
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/app/planlists');
+
+  $ionicConfigProvider.navBar.alignTitle('center');
 })
 
 .filter('format4yyyymd', function() {
